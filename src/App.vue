4bd91080e28a7022f3cb61bfe38d0ac33a601448
henriquet31 <henriquet023@outlook.com>
2025-05-15 18:13:45 -0300

    Adicionar o Bootstrap para estilizar e implementar a funcionalidade básica da calculadora.

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,57 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, computed } from 'vue';
+
+const estado = reactive({
+  num1: 0,
+  num2: 0,
+  operacao: ''
+});
+
+const resultado = computed(() => {
+  const { num1, num2, operacao } = estado;
+  switch (operacao) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      return num2 !== 0 ? num1 / num2 : 'Erro (divisão por zero)';
+    default:
+      return 'Operação inválida';
+  }
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container pb-5 bg-light rounded-3 shadow-sm" style="max-width: 600px;">
+    <header class="p-5 mb-2 mt-4">
+      <h1 class="text-center">Calculadora</h1>
+    </header>
+    <form>
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <select v-model="estado.operacao" class="form-select mb-3 text-center w-100">
+            <option value="+">Soma</option>
+            <option value="-">Subtração</option>
+            <option value="*">Multiplicação</option>
+            <option value="/">Divisão</option>
+          </select>
+        </div>
+        <div class="col-md-8">
+          <input v-model.number="estado.num1" type="number" class="form-control mb-3 text-center w-100" placeholder="Digite um número">
+        </div>
+        <div class="col-md-8">
+          <input v-model.number="estado.num2" type="number" class="form-control mb-3 text-center w-100" placeholder="Digite um número">
+        </div>
+      </div>
+    </form>
+    <div class="mt-4 text-center">
+      <h3>Resultado: {{ resultado }}</h3>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
